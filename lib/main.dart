43c85a735e4617944944da6a71fce4c434d14ffa
jantapabh <s6035512034@phuket.psu.ac.th>
2020-03-24 12:12:24 +0700

    Test Commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,3 +2,31 @@ import 'package:flutter/material.dart'
 
  void main() => runApp(MyApp());
 
+var headerSection = Image.asset('assets/images/coo.jpg');
+
+
+var middleSection = Container(
+
+
+
+);
+
+
+var lastSection = Container(
+
+
+);
+
+
+class MyApp extends StatelessWidget{
+
+ @override
+
+ Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+
+
+    );
+  }
+}
